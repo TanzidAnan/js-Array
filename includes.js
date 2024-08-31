7ffc580c65a9baac0def8ddabcfbd47834b05f64
TanzidAnan <tanzid.anan125@gmail.com>
2024-08-31 16:39:10 +0600

    includes array

---
+++ includes.js
@@ -0,0 +1,9 @@
+const numbers =[54, 68, 36, 78, 25, 47, 36];
+console.log(numbers.includes(36));
+
+if(numbers.includes(36)){
+    console.log('Thaka nai')
+}
+else{
+    console.log('Thaka dai')
+}
