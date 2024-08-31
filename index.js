bca6aa3412e4422796ee9d863b6548b062c3b274
TanzidAnan <tanzid.anan125@gmail.com>
2024-08-31 16:23:06 +0600

    push array

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const forth = numbers[2];
 console.log(forth);
 
 numbers[2] =569;
-console.log(numbers)
+console.log(numbers) 
---
+++ push-pop.js
@@ -0,0 +1,7 @@
+const age =[];
+const numbers =[54, 68, 36, 78, 25, 47, 36];
+
+console.log(numbers)
+
+numbers.push(561);
+console.log(numbers)
