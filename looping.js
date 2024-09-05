b38a6125b476922eabdfc76b8fda659ea1eaa17f
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-05 23:53:03 +0600

    while looping

--- looping.js
+++ looping.js
@@ -1,4 +1,11 @@
 const numbers = [21, 32, 65, 98, 45, 35, 65, 78, 45, 69];
 for(let i = 0; i<numbers.length ; i++){
-    console.log(numbers[i])
+    // console.log(numbers[i])
+}
+
+
+let i =0;
+while(i< numbers.length){
+    console.log(numbers[i]);
+    i++;
 }
