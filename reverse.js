e566cd4b063a0bd7bd7f49577157e06c1b23e8bd
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-06 00:31:40 +0600

    reversr push array number

---
+++ reverse.js
@@ -0,0 +1,12 @@
+const numbers = [25, 68, 45, 98, 78, 45, 65, 78, 45];
+// numbers.reverse();
+// console.log(numbers)
+
+
+let rev_number = [];
+for(const num of numbers){
+    // console.log(num);
+    rev_number.push(num)
+}
+console.log(rev_number.reverse())
+console.log(numbers)
