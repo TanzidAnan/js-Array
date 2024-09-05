efc890017fc1285060d05f8507de9e80bb5993d1
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-06 00:34:22 +0600

    array unshift

--- reverse.js
+++ reverse.js
@@ -6,7 +6,7 @@ const numbers = [25, 68, 45, 98, 78, 45, 65, 78, 45];
 let rev_number = [];
 for(const num of numbers){
     // console.log(num);
-    rev_number.push(num)
+    rev_number.unshift(num)
 }
-console.log(rev_number.reverse())
+console.log(rev_number)
 console.log(numbers)
