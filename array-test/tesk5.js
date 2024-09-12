8a7842e41aeaae99ead63613aa9b3eb311ac2d01
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-12 17:24:25 +0600

    tesk 5

---
+++ array-test/tesk5.js
@@ -0,0 +1,11 @@
+// 5. Combining Arrays
+// Instructions:
+
+// Create two arrays of your choice.
+// Use the concat method to combine the two arrays into a new array.
+// Print both the original arrays and the combined array using console.log().
+
+const number1 =[54, 65, 98, 75]
+const number2 =[98, 35, 10, 36];
+const totalNunber =(number1.concat(number2));
+console.log(totalNunber)
